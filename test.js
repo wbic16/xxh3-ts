fc33d2345fa99896b66348d9d056ded57cecc269
Will Bickford <wbic16@gmail.com>
2024-09-08 15:19:59 -0500

    had a couple of minutes

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-require('./xxh3.ts');
+require('buffer');
+var imports = require('./xxh3.ts');
+var n = function (n) { return BigInt(n); };
 var data = Buffer.from('example');
 var key64 = Buffer.from('0000');
 var seed = n(0);
-var result = XXH3_len_9to16_128b(data, key64, seed);
+var result = imports.XXH3_len_9to16_128b(data, key64, seed);
 console.log(result);
--- test.ts
+++ test.ts
@@ -1,7 +1,9 @@
-require('./xxh3.ts');
+require('buffer');
+var imports = require('./xxh3.ts');
 
+const n = (n: number | string) => BigInt(n);
 var data = Buffer.from('example');
 var key64 = Buffer.from('0000');
 var seed = n(0);
-var result = XXH3_len_9to16_128b(data, key64, seed);
+var result = imports.XXH3_len_9to16_128b(data, key64, seed);
 console.log(result);
